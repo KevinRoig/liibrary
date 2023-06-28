0fb616acbc5649ae020f8897bff4d7fafe1ae9d5
KevinRoig <kevinroig@hotmail.com>
2023-06-28 12:35:34 +0200

    answer to quest

---
+++ src/main/java/com/wcs/library/entity/Book.java
@@ -0,0 +1,59 @@
+package com.wcs.library.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long Id;
+    private String title;
+    private String author;
+    private String description;
+
+    public Book(Long id, String title, String author, String description) {
+        Id = id;
+        this.title = title;
+        this.author = author;
+        this.description = description;
+    }
+
+    public Book() {
+    }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
